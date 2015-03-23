34cc878e1f8c316d907ebf3785d711d1a10f9652
edwin-github <edwin-github@users.noreply.github.com>
2015-03-22 23:17:59 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -532,7 +532,8 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  var mP = document.querySelector("#movingPizzas1")
+  var mP = document.querySelector("#movingPizzas1");
+  console.log(screen,availHeight);
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
