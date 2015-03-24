c9f373953e76e8e37e7f5f75a3faf0d4db55f3e1
edwin-github <edwin-github@users.noreply.github.com>
2015-03-24 09:37:49 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -516,7 +516,7 @@ function updatePositions() {
   		  Math.sin((scrTop / 1250) + (3 % 5)),
   		  Math.sin((scrTop / 1250) + (4 % 5))
  		  ];
-
+    console.log("itemsLen: " + itemsLen);
   for (var i = 0; i < itemsLen; i++) {
     var xAxis = items[i].basicLeft + 100 * phase[i%5] + 'px';
     items[i].style.transform = "translateX(" +xAxis+ ")";
